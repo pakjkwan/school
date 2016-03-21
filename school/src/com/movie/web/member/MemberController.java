d1a05aebe9b46e1e635e3904e74ea1d821c859c0
자기이름 <@naver.com>
2016-03-21 15:38:40 +0900

    .

--- school/src/com/movie/web/member/MemberController.java
+++ school/src/com/movie/web/member/MemberController.java
@@ -9,41 +9,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.movie.web.global.Action;
-import com.movie.web.global.ActionForward;
 import com.movie.web.global.Command;
 import com.movie.web.global.CommandFactory;
-import com.movie.web.global.Constants;
 
-@WebServlet("/member/login_form.do")
+@WebServlet({"/member/login_form.do","/member/join_form.do"})
 public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	// 페이지 이동시에는 doGet  
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	doProc(request, response);
     	System.out.println("인덱스에서 들어옴");
-    	CommandFactory fac = new CommandFactory();
+    	String path = request.getServletPath();
+		String temp = path.split("/")[2];
+		String directory = path.split("/")[1];
+	//	arr[1] = temp3.split("\\.")[0]; 이 방법도 가능
+		String action = temp.substring(0, temp.indexOf("."));
+		
+		System.out.println("디렉토리"+directory);
+		System.out.println("액션"+action);
+		Command command = CommandFactory.createCommand(directory,action);
+		
 		RequestDispatcher dis = 
-			//	request.getRequestDispatcher(new Command(doProc(request,response)[0], doProc(request,response)[1]).getView());
-				request.getRequestDispatcher(fac.getCommand(doProc(request,response)[0], doProc(request,response)[1]).getView());
+		request.getRequestDispatcher(command.getView());
 		dis.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-	}
-	
-	public String[] doProc(HttpServletRequest request, HttpServletResponse response){
-		String[] arr = new String[2];
-		String path = request.getServletPath();
-		String temp = path.split("/")[2];
-		
-		arr[0] = path.split("/")[1];
-	//	arr[1] = temp3.split("\\.")[0]; 이 방법도 가능
-		arr[1] = temp.substring(0, temp.indexOf("."));
-		
-		return arr;
 	}
 
 }
