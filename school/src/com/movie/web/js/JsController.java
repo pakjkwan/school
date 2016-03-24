447c8b18cd2954bc8910a469361ae5a7f6bd8633
자기이름 <@naver.com>
2016-03-24 09:45:48 +0900

    .

--- school/src/com/movie/web/js/JsController.java
+++ school/src/com/movie/web/js/JsController.java
@@ -7,21 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet({"/js/hello.do"})
+@WebServlet({ "/js/hello.do" })
 public class JsController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-  
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
 	}
 
 }
