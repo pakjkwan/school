d9cddfc7000c4e8443511f779497e283e70351eb
자기이름 <@naver.com>
2016-03-24 10:43:55 +0900

    .

--- school/src/com/movie/web/js/JsController.java
+++ school/src/com/movie/web/js/JsController.java
@@ -6,14 +6,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.movie.web.global.Command;
 import com.movie.web.global.CommandFactory;
 import com.movie.web.global.DispatcherServlet;
 import com.movie.web.global.Seperator;
-import com.movie.web.member.MemberBean;
 
-@WebServlet({ "/js/hello.do" })
+@WebServlet({ "/js/hello.do","/js/var.do","/js/operator.do",
+	"/js/function.do","/js/object.do","/js/bom.do",
+	"/js/dom.do","/js/form.do","/js/closure.do",
+	"/js/pattern.do"})
 public class JsController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
